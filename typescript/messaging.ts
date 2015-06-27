23421c584cd44377d0a6b538140f9395035a7243
ljw <liamv7@gmail.com>
2015-06-27 19:02:05 +1200

    update

---
+++ typescript/messaging.ts
@@ -0,0 +1,50 @@
+/// <reference path="../typings/chrome/chrome.d.ts"/>
+'use strict';
+
+class Messaging {
+    port_ = null;
+    //[   dispatcher_: string] : string;//  = new Map<string, string>();
+    // x : Map<string, string>;
+    //dispatcherxx_ : Map<String, Function>;
+    dispatcher_ : Array<Function> = new Array<Function>();
+    constructor() {
+        //this.dispatcher_ = new Map<String, Function>();
+
+    }
+
+    CreateListener():void {
+        chrome.runtime.onConnect.addListener((port:chrome.runtime.Port) => {
+            console.log("received connect from " + port.name);
+            this.port_ = port;
+            console.log("what is OnReceive " + this.OnReceive);
+            port.onMessage.addListener((msg:any) => {
+                console.log("OnReceive " + msg.action);
+                var action = this.dispatcher_[msg.action];
+                console.log("disp " + action);
+                if (action != undefined) {
+                    action();
+                }
+            });
+            port.postMessage({action: "Listening"});
+        });
+    }
+
+
+    OnReceive(msg : any):void {
+        console.log("OnReceive " + msg.action);
+        var action = this.dispatcher_[msg.action];
+        if (action != undefined) {
+            action.call(null);
+            //action.caller.call();
+        }
+    }
+
+    SendInitiated():void {
+        this.port_.postMessage({action: "InitDone"});
+    }
+
+    Subscribe(name:string, callback: Function):void {
+        console.log("Messaging.prototype.Subscribe");
+        this.dispatcher_[name] = callback;
+    }
+}
---
+++ typescript/popup.ts
@@ -0,0 +1,45 @@
+/// <reference path="../typings/chrome/chrome.d.ts"/>
+/// <reference path="tabpage.ts" />
+'use strict';
+
+module Popup {
+    export class Popup {
+        private popup_: Element;
+        klcss_:Element;
+        tab_:chrome.tabs.Tab;
+
+        constructor() {
+
+        }
+
+        Display(htmlElement: Node): void {
+            // TODO use: chrome.tabs.insertCSS
+            console.log("Display");
+            this.klcss_ = window.document.createElementNS('http://www.w3.org/1999/xhtml', 'link');
+            this.klcss_.setAttribute('rel', 'stylesheet');
+            this.klcss_.setAttribute('type', 'text/css');
+            var cssFile = chrome.extension.getURL('klstyle.css');
+            console.log(cssFile);
+            this.klcss_.setAttribute('href', cssFile);
+            this.klcss_.setAttribute('id', 'klstyle-css');
+            window.document.getElementsByTagName('head')[0].appendChild(this.klcss_);
+
+            this.popup_ = window.document.createElementNS("http://www.w3.org/1999/xhtml", "div");
+            this.popup_.setAttribute('id', 'klstyle')
+            this.popup_.appendChild(htmlElement);
+            window.document.documentElement.appendChild(this.popup_);
+            //this.popup_.style.left = 90;
+            //this.popup_.style.top = 50;
+            //this.popup_.style.display = '';
+        }
+
+        Clear() : void {
+            console.log("clear")
+            if (this.popup_) {
+                this.popup_.parentNode.removeChild(this.popup_);
+                this.klcss_.parentNode.removeChild(this.popup_);
+            }
+        }
+    }
+}
+
